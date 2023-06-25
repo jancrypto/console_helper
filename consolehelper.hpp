90f97cf8e543e806b21fb406ed761b30761108ec
Jan <96467770+jancrypto@users.noreply.github.com>
2023-06-25 13:17:53 +0200

    Update consolehelper.hpp

--- consolehelper.hpp
+++ consolehelper.hpp
@@ -4,15 +4,15 @@
 
 namespace console_helper {
 
-	void reset_color() {
+	inline void reset_color() {
 		(void)SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE| FOREGROUND_GREEN | FOREGROUND_BLUE);
 	}
 
-	void set_color(unsigned int color) {
+	inline void set_color(unsigned int color) {
 		(void)SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 	}
 
-	void set_size(int width, int height) {
+	inline void set_size(int width, int height) {
 		HWND consoleWindow = GetConsoleWindow();
 
 		RECT windowRect;
@@ -22,7 +22,7 @@ namespace console_helper {
 	}
 
 
-	void set_console_font(LPCWSTR fontName, int sizeY, int sizeX) {
+	inline void set_console_font(LPCWSTR fontName, int sizeY, int sizeX) {
 		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 		CONSOLE_FONT_INFOEX fontInfo;
@@ -37,11 +37,11 @@ namespace console_helper {
 		(void)SetCurrentConsoleFontEx(hConsole, FALSE, &fontInfo);
 	}
 
-	void set_console_font(LPCWSTR fontName, int sizeY) {
+	inline void set_console_font(LPCWSTR fontName, int sizeY) {
 		set_console_font(fontName, sizeY, 0);
 	}
 
-	void set_console_font(LPCWSTR fontName) {
+	inline void set_console_font(LPCWSTR fontName) {
 		set_console_font(fontName, 20);
 	}
 }
